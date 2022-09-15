367cdba3f39b7659f7d92b3cc1cf189ea5137920
SuperYannick <yannick.chenais@hotmail.fr>
2022-09-15 11:13:11 +0200

    Challenge3 routing avancé

--- src/Controller/ProgramController.php
+++ src/Controller/ProgramController.php
@@ -1,17 +1,26 @@
 <?php
 
-namespace App\Controller; 
+namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+#[Route('/program', name: 'program_')]
 class ProgramController extends AbstractController
 {
-    #[Route('/program/', name: 'program_index')]
-    public function index() : Response 
+    // #[Route('/', name: '_index')]
+    // public function index(): Response
+    // {
+    //     return $this->render('program/index.html.twig', [
+    //         'website' => 'Donkey Séries'
+    //     ]);
+    // }
+    #[Route('/{id<\d+>}', name: 'show', methods: ['GET'])]
+    public function show(int $id = 1): Response
     {
-        return $this->render('program/index.html.twig', [
+        return $this->render('program/show.html.twig', [
+            'id' => $id,
             'website' => 'Donkey Séries'
         ]);
     }
