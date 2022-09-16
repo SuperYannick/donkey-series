5d2cd2e5664c4be5cfdd6c8d78ac49aafc6ddeef
SuperYannick <yannick.chenais@hotmail.fr>
2022-09-16 12:13:10 +0200

    creation CategoryController et vue twig finOneBy categoryName et findBy category

---
+++ src/Controller/CategoryController.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\ProgramRepository;
+use App\Repository\CategoryRepository;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+#[Route('/category', name: 'category_')]
+class CategoryController extends AbstractController
+{
+    #[Route('/', name: 'index')]
+    public function index(CategoryRepository $categoryRepository): Response
+    {
+        $categories = $categoryRepository->findAll();
+
+        return $this->render('category/index.html.twig', [
+            'categories' => $categories
+        ]);
+    }
+    #[Route('/{categoryName}', name: 'show')]
+    public function show(CategoryRepository $categoryRepository, string $categoryName, ProgramRepository $programRepository): Response
+    {
+        $category = $categoryRepository->findOneBy(['name' => $categoryName]);
+        $programs = $programRepository->findby(['category' => $category]);
+
+        if (!$category) {
+            throw $this->createNotFoundException(
+                'No category with name : ' . $categoryName . ' found in category\'s table.'
+            );
+        }
+
+        return $this->render('category/show.html.twig', [
+            'category' => $category,
+            'programs' => $programs
+        ]);
+    }
+}
